77a663eaf4b25541a6c935f764fa8fc5f1b39347
Jason Zhang <jasonzhang56@gmail.com>
2023-06-26 16:19:42 -0700

    change epoch size

--- finetune/adapter_v2.py
+++ finetune/adapter_v2.py
@@ -40,7 +40,7 @@ batch_size = 128 / devices
 micro_batch_size = 2  # set to 2 because this is fit into 12GB Vram
 gradient_accumulation_iters = batch_size // micro_batch_size
 assert gradient_accumulation_iters > 0
-epoch_size = 50000  # train dataset size
+epoch_size = 10000  # train dataset size
 num_epochs = 5
 max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
 weight_decay = 0.02
